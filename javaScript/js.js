e4a74c2a7bcf969e39ffb87e2c257a7f3753ff22
Only-Czesio <marek@kamyk.me>
2023-08-17 07:49:54 +0200

    added corrected styling for chart, added readData from data.json and created function for current day to color it different placed everything that it look almost the same

--- javaScript/js.js
+++ javaScript/js.js
@@ -7,17 +7,18 @@ const tableOfDays = [
   "Friday",
   "Saturday",
 ];
-const curr = document.querySelector(".currentDay");
 const columns = document.querySelectorAll("[data-square");
+const mon = document.querySelector("#Monday");
+const tue = document.querySelector("#Tuesday");
+const wed = document.querySelector("#Wednesday");
+const thu = document.querySelector("#Thursday");
+const fri = document.querySelector("#Friday");
+const sat = document.querySelector("#Saturday");
 const sun = document.querySelector("#Sunday");
+
 const d = new Date();
 let day = d.getDay();
-console.log(day);
 
-const addDate = () => {
-  console.log("Something is happening");
-  curr.innerHTML = tableOfDays[day];
-};
 const colorCurrentDay = () => {
   console.log("I'm changing color of current day");
     for (let i = 0; i < columns.length; i++) {
@@ -27,16 +28,76 @@ const colorCurrentDay = () => {
     }
 };
 const readData = () => {
+  try {
     fetch("../data/data.json")
-        .then((response) => response.json())
-        .then((data) => {
-            console.log(data);
-            sun.setAttribute("style",  "--size: calc( 5 / 100 );")
-        });
-    }
-    .catch((error) => {
-      console.error("error reading data.json",error);
-    });
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[0].amount);
+        mon.setAttribute("style", `--size: calc( ${data[0].amount} / 60 );`);
+      });
+  } catch (error) {
+    console.error("error reading data.json", error);
+  }
+  try {
+    fetch("../data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[1].amount);
+        tue.setAttribute("style", `--size: calc( ${data[1].amount} / 60 );`);
+      });
+  } catch (error) {
+    console.error("error reading data.json", error);
+  }
+  try {
+    fetch("../data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[2].amount);
+        wed.setAttribute("style", `--size: calc( ${data[2].amount} / 60 );`);
+      });
+  } catch (error) {
+    console.error("error reading data.json", error);
+  }
+  try {
+    fetch("../data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[3].amount);
+        thu.setAttribute("style", `--size: calc( ${data[3].amount} / 60 );`);
+      });
+  } catch (error) {
+    console.error("error reading data.json", error);
+  }
+  try {
+    fetch("../data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[4].amount);
+        fri.setAttribute("style", `--size: calc( ${data[4].amount} / 60 );`);
+      });
+  } catch (error) {
+    console.error("error reading data.json", error);
+  }
+  try {
+    fetch("../data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[5].amount);
+        sat.setAttribute("style", `--size: calc( ${data[5].amount} / 60 );`);
+      });
+  } catch (error) {
+    console.error("error reading data.json", error);
+  }
+  try {
+    fetch("../data/data.json")
+      .then((response) => response.json())
+      .then((data) => {
+        console.log(data[6].amount);
+        sun.setAttribute("style", `--size: calc( ${data[6].amount} / 60 );`);
+      });
+  } catch (error) {
+    console.error("error reading data.json", error);
+  }
+}
 readData();
 colorCurrentDay();
-addDate();
